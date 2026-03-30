834197bb3b601edb8f1615891a8f58ab9c590625
robertshein <rober@DESKTOP-BDT2S8O>
2026-03-30 11:06:20 +0500

    Разработанная модель представления главного окна. Шейн

---
+++ ViewModels/VM_Pages.cs
@@ -0,0 +1,25 @@
+﻿using praktika28_Shein.Classes;
+using praktika28_Shein.View;
+using System.Windows;
+
+namespace praktika28_Shein.ViewModels
+{
+    public class VM_Pages : Notification
+    {
+        public VM_Tasks vm_tasks = new VM_Tasks();
+        public VM_Pages()
+        {
+            MainWindow.init.frame.Navigate(new View.Main(vm_tasks));
+        }
+        public RelayCommand OnClose
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    MainWindow.init.Close();
+                });
+            }
+        }
+    }
+}
