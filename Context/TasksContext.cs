a952036bb107788aada4d1950d15984c2109b3d7
robertshein <rober@DESKTOP-BDT2S8O>
2026-03-30 11:03:44 +0500

    Разработанный контекст данных для модели задач. Шейн

---
+++ Context/TasksContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using praktika28_Shein.Classes.Database;
+using praktika28_Shein.Models;
+using System.Linq;
+
+namespace praktika28_Shein.Context
+{
+    public class TaskContext : DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+
+        public TaskContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(Config.connection, Config.version);
+        }
+    }
+}
