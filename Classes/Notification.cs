d651f961aabcece293b3060c43792ae56ae1cda0
robertshein <rober@DESKTOP-BDT2S8O>
2026-03-30 10:57:04 +0500

    Разработанный классы общего-пользования. Шейн

---
+++ Classes/Notification.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace praktika28_Shein.Classes
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string prop = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
